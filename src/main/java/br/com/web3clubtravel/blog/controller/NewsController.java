90ae87a5c2a545b103e091fc4404489cb8d726d0
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-04-14 10:11:06 -0300

    news endpoint details

--- src/main/java/br/com/web3clubtravel/blog/controller/NewsController.java
+++ src/main/java/br/com/web3clubtravel/blog/controller/NewsController.java
@@ -18,9 +18,17 @@ import br.com.web3clubtravel.blog.response.ResponseBase;
 import br.com.web3clubtravel.blog.response.ResponseBasePaginado;
 import br.com.web3clubtravel.blog.service.NewsService;
 import jakarta.transaction.Transactional;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @RestController
 @RequestMapping("/news")
+@Tag(name = "News", description = "Endpoints for consulting and create news")
 public class NewsController {
 
     private final NewsService newsService;
@@ -31,6 +39,23 @@ public class NewsController {
     }
 
     @GetMapping
+    @Operation(summary = "responsible for list new news ordered by creation date", description = "list new news ordered by creation date", 
+                tags = { "News" }, 
+                responses = { 
+                    @ApiResponse(description = "Success", responseCode = "200", 
+                        content = {
+                            @Content(mediaType = "application/json", 
+                                array = 
+                                    @ArraySchema(schema = @Schema(implementation = NewsDto.class))
+                                    )
+                                }
+                            ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+                    }
+                )
     public ResponseEntity<ResponseBasePaginado<List<NewsDto>>> listNews(
             @PageableDefault(sort = "dateTime", direction = Direction.ASC) Pageable pageable) {
         final var response = this.newsService.listNews(pageable);
@@ -40,6 +65,15 @@ public class NewsController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Add a new News", description = "Add a new News", 
+        tags = { "News" }, 
+        responses = {
+            @ApiResponse(description = "Create", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = NewsDto.class))),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+        )
     public ResponseEntity<ResponseBase<NewsDto>> save(@RequestBody NewsDto newsDto) {
         final var response = this.newsService.save(newsDto);
         final var base = ResponseBasePaginado.of(response);

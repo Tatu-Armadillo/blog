561e1e910e02b5849aa9430bcf26b74dde037c97
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-04-14 09:58:11 -0300

    traveler endpoint details

--- src/main/java/br/com/web3clubtravel/blog/controller/TravelerController.java
+++ src/main/java/br/com/web3clubtravel/blog/controller/TravelerController.java
@@ -11,9 +11,15 @@ import br.com.web3clubtravel.blog.dto.TravelerDto;
 import br.com.web3clubtravel.blog.response.ResponseBase;
 import br.com.web3clubtravel.blog.service.TravelerService;
 import jakarta.transaction.Transactional;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/traveler")
+@Tag(name = "Traveler", description = "Endpoints for Create a new traveler")
 public class TravelerController {
 
     private final TravelerService travelerService;
@@ -25,6 +31,15 @@ public class TravelerController {
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Add a new traveler", description = "Add a new traveler", 
+        tags = { "Traveler" }, 
+        responses = {
+            @ApiResponse(description = "Create", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TravelerDto.class))),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+        )
     public ResponseEntity<ResponseBase<TravelerDto>> save(
             @RequestBody TravelerDto dto) {
         final var response = this.travelerService.save(dto);

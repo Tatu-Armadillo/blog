391398ed510f6e59d5d0d7b720077e1ff2aa0305
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-04-14 10:19:57 -0300

    destination endpoint details

--- src/main/java/br/com/web3clubtravel/blog/controller/DestinationsController.java
+++ src/main/java/br/com/web3clubtravel/blog/controller/DestinationsController.java
@@ -19,9 +19,16 @@ import br.com.web3clubtravel.blog.response.ResponseBase;
 import br.com.web3clubtravel.blog.response.ResponseBasePaginado;
 import br.com.web3clubtravel.blog.service.DestinationService;
 import jakarta.transaction.Transactional;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/destinations")
+@Tag(name = "Destinations", description = "Endpoints for consulting and create destinations")
 public class DestinationsController {
 
     private final DestinationService destinationService;
@@ -32,16 +39,42 @@ public class DestinationsController {
     }
 
     @GetMapping
+    @Operation(summary = "responsible for list destinations ordered by city name", description = "list destinations ordered by city name", 
+    tags = { "Destinations" }, 
+    responses = { 
+        @ApiResponse(description = "Success", responseCode = "200", 
+            content = {
+                @Content(mediaType = "application/json", 
+                    array = 
+                        @ArraySchema(schema = @Schema(implementation = DestinationsDto.class))
+                        )
+                    }
+                ),
+        @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+        @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+        @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+        @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+        }
+    )
     public ResponseEntity<ResponseBasePaginado<List<DestinationsDto>>> listDestinationsWithFilter(
             @PageableDefault(sort = "city.name", direction = Direction.ASC) Pageable pageable,
-            @RequestParam(required = false, defaultValue = "") final String filter) {
-        final var response = this.destinationService.listDestinationsWithFilter(pageable, filter);
+            @RequestParam(required = false, defaultValue = "") final String cityName) {
+        final var response = this.destinationService.listDestinationsWithFilter(pageable, cityName);
         final var base = ResponseBasePaginado.of(response);
         return ResponseEntity.ok(base);
     }
 
     @PostMapping
     @Transactional
+    @Operation(summary = "Add a new destinations", description = "Add a new destinations", 
+        tags = { "Destinations" }, 
+        responses = {
+            @ApiResponse(description = "Create", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = DestinationsDto.class))),
+            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+        )
     public ResponseEntity<ResponseBase<DestinationsDto>> save(
             @RequestBody DestinationsDto dto) {
         final var destination = this.destinationService.save(dto);

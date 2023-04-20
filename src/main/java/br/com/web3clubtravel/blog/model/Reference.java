1c054552b67a8b369837d4dd15d425c672102a06
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-04-20 10:00:42 -0300

    mapped Reference

---
+++ src/main/java/br/com/web3clubtravel/blog/model/Reference.java
@@ -0,0 +1,83 @@
+package br.com.web3clubtravel.blog.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reference")
+public class Reference {
+
+    @Id
+    @Column(name = "id_reference")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idReference;
+
+    @Column(name = "key_reference")
+    private String keyReference;
+
+    @Column(name = "valor")
+    private String valor;
+
+    @Column(name = "image_link")
+    private String imageLink;
+
+    @Column(name = "image")
+    private byte[] image;
+
+    public Reference() { }
+
+    public Reference(Long idReference, String keyReference, String valor, String imageLink, byte[] image) {
+        this.idReference = idReference;
+        this.keyReference = keyReference;
+        this.valor = valor;
+        this.imageLink = imageLink;
+        this.image = image;
+    }
+
+    public Long getIdReference() {
+        return idReference;
+    }
+
+    public void setIdReference(Long idReference) {
+        this.idReference = idReference;
+    }
+
+    public String getKeyReference() {
+        return keyReference;
+    }
+
+    public void setKeyReference(String keyReference) {
+        this.keyReference = keyReference;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    
+
+}

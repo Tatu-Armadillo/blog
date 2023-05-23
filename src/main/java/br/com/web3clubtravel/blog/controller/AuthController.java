25096d3948ffa013cf0f9da7bb2585d83a281d1b
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-23 09:23:29 -0300

    fix-manipulate token

--- src/main/java/br/com/web3clubtravel/blog/controller/AuthController.java
+++ src/main/java/br/com/web3clubtravel/blog/controller/AuthController.java
@@ -2,53 +2,53 @@ package br.com.web3clubtravel.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.web3clubtravel.blog.config.exception.NegocioException;
-import br.com.web3clubtravel.blog.dto.UserDto;
+import br.com.web3clubtravel.blog.config.security.TokenService;
+import br.com.web3clubtravel.blog.model.User;
+import br.com.web3clubtravel.blog.record.auth.TokenRecord;
+import br.com.web3clubtravel.blog.record.auth.UserRecord;
 import br.com.web3clubtravel.blog.response.ResponseBase;
-import br.com.web3clubtravel.blog.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 @Tag(name = "Auth", description = "Endpoints for Login in application")
 public class AuthController {
 
-    private final UserService userService;
-    private static final String message = "Invalid client request!";
+    private final AuthenticationManager manager;
+    private final TokenService tokenService;
 
     @Autowired
-    public AuthController(final UserService userService) {
-        this.userService = userService;
+    public AuthController(final AuthenticationManager manager, final TokenService tokenService) {
+        this.manager = manager;
+        this.tokenService = tokenService;
     }
 
     @PostMapping("/signin")
-    @Operation(summary = "Make a request to the application", description = "Make a request to the application", 
-        tags = { "Auth" }, 
-        responses = {
-            @ApiResponse(description = "Create", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserDto.class))),
-            @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
-            @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
-            @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
-            }
-        )
-    public ResponseEntity<ResponseBase<UserDto>> signin(@RequestBody UserDto data) {
-        if (data == null
-                || data.getUsername() == null || data.getUsername().isBlank()
-                || data.getPassword() == null || data.getPassword().isBlank()) {
-            throw new NegocioException(message);
-        }
+    @Operation(summary = "Make a request to the application", description = "Make a request to the application", tags = {
+            "Auth" }, responses = {
+                    @ApiResponse(description = "Create", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = TokenRecord.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
+    public ResponseEntity<ResponseBase<TokenRecord>> signin(@Valid @RequestBody UserRecord data) {
+        final var authenticationToken = new UsernamePasswordAuthenticationToken(data.username(), data.password());
+        final var authentication = manager.authenticate(authenticationToken);
+        final var tokenJWT = tokenService.createToken((User) authentication.getPrincipal());
 
-        final var reponse = this.userService.previaLogin(data);
-        final var base = ResponseBase.of(reponse);
+        final var base = ResponseBase.of(new TokenRecord(tokenJWT));
         return ResponseEntity.ok(base);
     }
 
--- src/main/java/br/com/web3clubtravel/blog/service/UserService.java
+++ src/main/java/br/com/web3clubtravel/blog/service/UserService.java
@@ -1,46 +1,37 @@
 package br.com.web3clubtravel.blog.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import br.com.web3clubtravel.blog.config.exception.NegocioException;
-import br.com.web3clubtravel.blog.config.exception.NotFoundException;
-import br.com.web3clubtravel.blog.dto.UserDto;
 import br.com.web3clubtravel.blog.model.User;
-import br.com.web3clubtravel.blog.repository.PermissionRepository;
 import br.com.web3clubtravel.blog.repository.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
-    private final PermissionRepository permissionRepository;
 
     @Autowired
     public UserService(
-            final UserRepository userRepository,
-            final PermissionRepository permissionRepository) {
+            final UserRepository userRepository) {
         this.userRepository = userRepository;
-        this.permissionRepository = permissionRepository;
     }
 
     public User save(User user) {
-        final var permission = this.permissionRepository.getPermissionByDescription("traveler");
-        user.setPermissions(List.of(permission));
         final var reponse = this.userRepository.save(user);
         return reponse;
     }
 
-    public UserDto previaLogin(UserDto dto) {
-        final User user = this.userRepository.findByUsername(dto.getUsername())
-                .orElseThrow(() -> new NotFoundException("User not Found with username = " + dto.getUsername()));
-                
-        if (!user.getPassword().equals(dto.getPassword())) {
-            throw new NegocioException("Incorrect Password");
-        }
-        return UserDto.of(user);
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username);
+    }
+
+    public UserDetails findByUsername(String username) {
+        return this.userRepository.findByUsername(username);
     }
 
 }
